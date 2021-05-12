0cf92bfa635c6a313a5912f3642ecf18d9bfe993
TecCheck <mcteccy@protonmail.com>
2021-05-12 16:17:45 +0200

    Add empty ItemEditActivity

---
+++ app/src/main/java/io/github/storagereloaded/android/ui/ItemEditActivity.java
@@ -0,0 +1,16 @@
+package io.github.storagereloaded.android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.storagereloaded.android.R;
+
+public class ItemEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_edit);
+    }
+}
